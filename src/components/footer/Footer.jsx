4651069674698ab0d7a4a0e451d3e2792e771fc6
Erasmo Jr <erasmo.cjunior@gmail.com>
2023-01-03 17:31:42 -0300

    :tada: FEAT: First pages

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './Footer.css';
+
+function Footer() {
+    return (<footer>
+            <p>Created by <a href="https://github.com/redmasters">Red Master</a></p>
+        </footer>
+    )
+}
+
+export default Footer;
